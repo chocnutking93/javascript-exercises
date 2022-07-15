7cda558b1ef66636ca037fcd3893bda1238a2303
Charles <ckelly218@gmail.com>
2022-07-14 23:22:15 -0700

    Added the .repeat function to string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,8 @@
-const repeatString = function() {
-
-};
+function repeatString(){
+   let greeting = 'hey';
+    return greeting.repeat(3);
+}
+repeatString();
 
 // Do not edit below this line
 module.exports = repeatString;
