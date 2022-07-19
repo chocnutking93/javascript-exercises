ce1d5e3df0e61420c9c6ad0039942452b7ddd78f
Charles <ckelly218@gmail.com>
2022-07-19 11:22:45 -0700

    Added while loop to function and if statement

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,8 +1,34 @@
-function repeatString(){
-   let greeting = 'hey';
-    return greeting.repeat(3);
-}
-repeatString();
+function repeatString(string, num) {
+    let repeatedString = "";
+    while (num > 0) {
+      repeatedString += string;
+      num--;
+    }
+    if (num <0){
+        return repeatedString = 'ERROR';
+    }
+    return repeatedString;
+  }
+
+ function randomString(string,y){
+    let y =  Math.floor(Math.random() * 1000)
+    let repeatedString = "";
+    while (y >= 0){
+        repeatedString += string;
+        y--;
+    }
+    return repeatedString;
+
+ }
+
+
+  
+  repeatString("hey", 10);
+  repeatString("hey",0);
+  repeatString("hey",-1);
+  repeatString("hey",number).match(/(hey)/g).length.toEqual(number);
+
+  
 
 // Do not edit below this line
 module.exports = repeatString;
