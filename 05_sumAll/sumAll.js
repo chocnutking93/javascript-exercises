26771255a7cbb48105ad1362569cbea4d12bc05a
Charles <ckelly218@gmail.com>
2022-08-02 07:11:30 -0700

    Added

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
-
+function sumAll(x, y){
+    let sum = 0;
+    if (!Number.isInteger(x) || !Number.isInteger(y))
+        return "ERROR";
+    if (x < 0 || y < 0) 
+        return "ERROR";
+    if (x > y){
+        let temp = x;
+        x = y;
+        y = temp;
+    }
+        
+    for (let i = x; i < y + 1; i ++){
+        sum +=i;
+    }
+   
+    return sum;
 };
-
+    
 // Do not edit below this line
-module.exports = sumAll;
+module.exports = sumAll
