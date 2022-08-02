74f9e25f5033148ac9f94ce831171dac60928279
Charles <ckelly218@gmail.com>
2022-08-02 06:37:03 -0700

    Input formula for each conversion, rounded to nearest decimal

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,16 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  let formula1 = ((f - 32) * (5/9));
+  let rounded = Math.round(formula1 * 10) / 10;
+  return rounded;
 };
+ 
 
-const ctof = function() {
-
+const ctof = function(c) {
+  let formula2 = ((c * 9/5) + 32);
+  let rounded2 = Math.round(formula2 *10) / 10;
+  return rounded2;
 };
+  
 
 // Do not edit below this line
 module.exports = {
